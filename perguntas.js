e6fdc40291480e5966d2123e23c81218708f911b
Lore melo <143742218+loremello@users.noreply.github.com>
2024-10-08 12:21:23 -0300

    rhianna

---
+++ perguntas.js
@@ -0,0 +1,101 @@
+criacartao(
+    'matematica',
+    'como se calcula soma dos termos de uma P.A?',
+    'utiliza-se a formula:((a1+an).q)/2'
+)
+//20 perguntas ao todo
+criacartao(
+    'geograia',
+    'o que el nino?',
+    'utiliza-se a formula: aquecimento anormal nas aguas do pacifico'
+)
+criacartao(
+    'Historia',
+    'quanto tempo durou a era vargas?',
+    'utiliza-se a formula:15 anos'
+)
+criacartao(
+    'quimica',
+    'o que é ligação covalente?',
+    'utiliza-se a formula:ligação entre ametais '
+)
+criacartao(
+    'português',
+    'o que é um verbo?',
+    'utiliza-se a formula:toda ação, estado ou fenomeno da natureza'
+)
+criacartao(
+    'biologia',
+    'qual e a celula reprodutora feminina?',
+    'utiliza-se a formula:ovulo'
+)
+criacartao(
+    'sociolocia',
+    'quem escreveu o manifesto do partido comunista?',
+    'utiliza-se a formula:Karl marx'
+)
+criacartao(
+    'empreendedorismo',
+    'o que é a FOFA',
+    'utiliza-se a formula:orça, oportunidade, fraqueza e ameaça'
+)
+criacartao(
+    'educação financeira',
+    'o que ignifica a sigla rdb?',
+    'utiliza-se a formula: Recibo de Depósito Bancário'
+)
+criacartao(
+    'historia?',
+    'em qual ano caiu o murro de Berlin?',
+    'utiliza-se a formula: 1989'
+)
+criacartao(
+    'matematica',
+    'o que é um numero primo??',
+    'utiliza-se a formula:um numero dividido por ele e por 1'
+)
+criacartao(
+    'geografia',
+    'Qual a capital da Venezuela?',
+    'utiliza-se a formula: Caracas'
+)
+criacartao(
+    'Geografia',
+    'onde está localizada a see da ONU?',
+    'utiliza-se a formula:Nova York'
+)
+criacartao(
+    'biologia',
+    'qual é o sangue doador universal?',
+    'utiliza-se a formula:O-'
+)
+criacartao(
+    'ingles',
+    'qual a tradução d I AM?',
+    'utiliza-se a formula:eu sou ou eu estou'
+)
+criacartao(
+    'biotecnologia',
+    'o que é a biottecnolia preta?',
+    'utiliza-se a formula:bioterrorismo'
+)
+criacartao(
+    'portugues',
+    'o que é um hiato?',
+    'utiliza-se a formula:encontro de vogais'
+)
+criacartao(
+    'geografia',
+    'o que significa URSS?',
+    'utiliza-se a formula:união das republicas socialistas sovieticas'
+)
+criacartao(
+    'geografica',
+    'qual maior rio o mundo?',
+    'utiliza-se a formula:rio amazonas'
+)
+criacartao(
+    'ingles',
+    'como se fala correr em ingles?',
+    'utiliza-se a formula:run'
+)
