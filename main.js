68a9f8a62d79241063314ad38187a2f1df478536
Lore melo <143742218+loremello@users.noreply.github.com>
2024-10-15 11:22:05 -0300

    lady gaga

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ function criaCartao (categoria, pergunta, resposta){
     cartao.innerHTML = `<div class="conteudo-cartao">
 <h3>categoria</h3>
 <div class="pergunta-cartao">
-<p>peergunta</p>
+<p>pergunta</p>
 </div>
 <div class="resposta-cartao">
 
