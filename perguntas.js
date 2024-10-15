81d3eddfe80bfe1b57f4405c98bd5eaaf6619b69
Lore melo <143742218+loremello@users.noreply.github.com>
2024-10-15 11:38:35 -0300

    BILLIE

--- perguntas.js
+++ perguntas.js
@@ -1,100 +1,100 @@
-criacartao(
+criaCartao(
     'matematica',
     'como se calcula soma dos termos de uma P.A?',
     'utiliza-se a formula:((a1+an).q)/2'
 )
 //20 perguntas ao todo
-criacartao(
+criaCartao(
     'geograia',
     'o que el nino?',
     'utiliza-se a formula: aquecimento anormal nas aguas do pacifico'
 )
-criacartao(
+criaCartao(
     'Historia',
     'quanto tempo durou a era vargas?',
     'utiliza-se a formula:15 anos'
 )
-criacartao(
+criaCartao(
     'quimica',
     'o que é ligação covalente?',
     'utiliza-se a formula:ligação entre ametais '
 )
-criacartao(
+criaCartao(
     'português',
     'o que é um verbo?',
     'utiliza-se a formula:toda ação, estado ou fenomeno da natureza'
 )
-criacartao(
+criaCartao(
     'biologia',
     'qual e a celula reprodutora feminina?',
     'utiliza-se a formula:ovulo'
 )
-criacartao(
+criaCartao(
     'sociolocia',
     'quem escreveu o manifesto do partido comunista?',
     'utiliza-se a formula:Karl marx'
 )
-criacartao(
+criaCartao(
     'empreendedorismo',
     'o que é a FOFA',
     'utiliza-se a formula:orça, oportunidade, fraqueza e ameaça'
 )
-criacartao(
+criaCartao(
     'educação financeira',
     'o que ignifica a sigla rdb?',
     'utiliza-se a formula: Recibo de Depósito Bancário'
 )
-criacartao(
+criaCartao(
     'historia?',
     'em qual ano caiu o murro de Berlin?',
     'utiliza-se a formula: 1989'
 )
-criacartao(
+criaCartao(
     'matematica',
     'o que é um numero primo??',
     'utiliza-se a formula:um numero dividido por ele e por 1'
 )
-criacartao(
+criaCartao(
     'geografia',
     'Qual a capital da Venezuela?',
     'utiliza-se a formula: Caracas'
 )
-criacartao(
+criaCartao(
     'Geografia',
     'onde está localizada a see da ONU?',
     'utiliza-se a formula:Nova York'
 )
-criacartao(
+criaCartao(
     'biologia',
     'qual é o sangue doador universal?',
     'utiliza-se a formula:O-'
 )
-criacartao(
+criaCartao(
     'ingles',
     'qual a tradução d I AM?',
     'utiliza-se a formula:eu sou ou eu estou'
 )
-criacartao(
+criaCartao(
     'biotecnologia',
     'o que é a biottecnolia preta?',
     'utiliza-se a formula:bioterrorismo'
 )
-criacartao(
+criaCartao(
     'portugues',
     'o que é um hiato?',
     'utiliza-se a formula:encontro de vogais'
 )
-criacartao(
+criaCartao(
     'geografia',
     'o que significa URSS?',
     'utiliza-se a formula:união das republicas socialistas sovieticas'
 )
-criacartao(
+criaCartao(
     'geografica',
     'qual maior rio o mundo?',
     'utiliza-se a formula:rio amazonas'
 )
-criacartao(
+criaCartao(
     'ingles',
     'como se fala correr em ingles?',
     'utiliza-se a formula:run'
